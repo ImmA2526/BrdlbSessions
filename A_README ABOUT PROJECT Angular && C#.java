3b3820eed5fdfb0dbeeb96b33b7819a05c29671b
unknown <imraninfo004@gmail.com>
2022-08-27 19:59:34 +0530

    ReadmeNote

---
+++ A_README ABOUT PROJECT Angular && C#.java
@@ -0,0 +1,84 @@
+---------------------C# Dotnet and SQL ---------------------------
+
+>> Create DotNetCore Web Api Project 2.1
+>> Include Liabray >> Right Click on Solution >> Add Project >> Class Liabray C# 2.1
+>> Add Refrence of the project (if necesssary ) 
+>> Install First >> Microsoft.NET.Test.Sdk
+>> install Project PAckage as per the Project Version
+>> Connect SQL with Visaul Studio 
+
+>> If Migration Then first Create Database Manually >> Nuget Package Manager Console >> Select Project where DBContext File >> Add Migration >> Update Database 
+        -- Add-Migration BOOK-STORE >> Name of Migration
+        -- Update-Database
+
+>> Connecting Local Database with SQL || Windows Authentication >> (localdb)\MSSQLLocalDB 
+>>
+
+
+IF the redis is connected install it first then start redis server.exe if its not started then start rediscli.exe and type ping it will give pong.
+
+
+
+-----------------------Angular------------------------
+
+Entry Point --- Main.ts >>> bootstrap(Module File) App.ts >>> Index.html >> Load HTML Templates  
+
+22 VIDEO COMPLETE********************************************
+
+<-------------- Angular -------------->
+
+----> Creae New Project 
+    ng new PROJECT NAME
+
+    OR 
+
+    ng new PROJECT NAME --routing --style css 
+
+----> Create Component 
+    ng g c COMPONENT NAME 
+
+----> Create Service 
+    ng g service SERVICE Filename
+
+<--------------- Json Server --------------->
+  
+--->Create and Run Json
+    json-server --watch DATAFILE NAME
+
+
+
+most of the call should give inside ngAfterViewInit() : Coz first view should be display
+
+
+<------ Reactive Form ------>
+
+
+1) Create Varibale and assign the FormGroup Object 
+2) Inject FormBuilder in Constructor Constructor
+3) Assign and initalise all value 
+4)  
+
+
+<------ DOTNET PROJECT IN VsCode ------>
+
+dotnet --info
+dotnet run   runnig project in vs code 
+dotnet --version
+
+WebApi in VsCode
+
+1-- terminal cd project 
+2-- dotnet new webapi --no-https   //creating project 
+3-- 
+
+
+
+
+
+
+ 
+Imran2526 
+Avenger27*   Norhtern Job
+
+
+wmic path softwareLicensingService get OA3xOriginalProductKey
